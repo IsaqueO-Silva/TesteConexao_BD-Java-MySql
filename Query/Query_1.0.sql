d5da8c393d89a952f0ece9c57d46b91eb573e4dc
Isaque Silva <46305573+IsaqueO-Silva@users.noreply.github.com>
2020-01-06 19:30:56 -0300

    Add files via upload

---
+++ Query/Query_1.0.sql
@@ -0,0 +1,12 @@
+create database db_teste;
+
+create table tb_teste
+(
+	id int primary key auto_increment,
+    nome varchar(80) not null,
+    fone varchar(50),
+    email varchar(50) unique,
+    idade int not null
+    
+);
+
