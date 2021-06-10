a02065a843bbb0a452f162e749336029ba97a6da
Isaque Silva <46305573+IsaqueO-Silva@users.noreply.github.com>
2021-06-10 08:45:25 -0300

    Update Query_1.0.sql

--- Query/Query_1.0.sql
+++ Query/Query_1.0.sql
@@ -2,7 +2,7 @@ create database db_teste;
 
 create table tb_teste
 (
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     nome varchar(80) not null,
     fone varchar(50),
     email varchar(50) unique,
